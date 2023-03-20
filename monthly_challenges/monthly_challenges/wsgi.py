4624ea132616ab07d5f3f209de42ec15b3cbda88
Restyled.io <commits@restyled.io>
2023-03-20 04:36:57 +0000

    Restyled by black

--- monthly_challenges/monthly_challenges/wsgi.py
+++ monthly_challenges/monthly_challenges/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monthly_challenges.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monthly_challenges.settings")
 
 application = get_wsgi_application()
