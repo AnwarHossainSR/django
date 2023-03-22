77536d30d9a2f3f8779830c66b27f7342e2408e2
Restyled.io <commits@restyled.io>
2023-03-22 11:32:47 +0000

    Restyled by black

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("<int:month>", views.monthly_challenge_by_month),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge),
 ]
