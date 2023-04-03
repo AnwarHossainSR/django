38d3dab4fb295a8cef7d693b55da4941d4ff2a69
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-04-03 14:26:30 +0600

    remove all project

--- monthly_challenges/challenges/urls.py
+++
@@ -1,7 +0,0 @@
-from challenges import views
-from django.urls import path
-
-urlpatterns = [
-    path("<int:month>", views.monthly_challenge_by_month),
-    path("<str:month>", views.monthly_challenge)
-]
