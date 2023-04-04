9977ecf0634c12d1f30e116b9669aacafc90dc48
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-04-04 12:50:05 +0600

    access data in html

--- developmentkit/developmentkit/views.py
+++ developmentkit/developmentkit/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render
 def index(request):
     data = {
         'title': 'Home Page',
+        'values': ['php', 'python', 'java', 'c++'],
+        'students': [
+            {'name': 'John', 'age': 18},
+            {'name': 'Bob', 'age': 20},
+            {'name': 'Alice', 'age': 19},
+            {'name': 'Kate', 'age': 21},
+        ]
     }
     return render(request, 'index.html', data)
 
