0b907acda237a8272b1877c0d61bbbdee6c84ac6
Restyled.io <commits@restyled.io>
2023-04-04 06:55:04 +0000

    Restyled by yapf

--- developmentkit/developmentkit/views.py
+++ developmentkit/developmentkit/views.py
@@ -4,13 +4,26 @@ from django.shortcuts import render
 
 def index(request):
     data = {
-        "title": "Home Page",
+        "title":
+        "Home Page",
         "values": ["php", "python", "java", "c++"],
         "students": [
-            {"name": "John", "age": 18},
-            {"name": "Bob", "age": 20},
-            {"name": "Alice", "age": 19},
-            {"name": "Kate", "age": 21},
+            {
+                "name": "John",
+                "age": 18
+            },
+            {
+                "name": "Bob",
+                "age": 20
+            },
+            {
+                "name": "Alice",
+                "age": 19
+            },
+            {
+                "name": "Kate",
+                "age": 21
+            },
         ],
     }
     return render(request, "index.html", data)
